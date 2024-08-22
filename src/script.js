abb6c21495e3642ffbe583ef3fbbee96be4b9391
FoxGalaxy2MishaKhodakov <132774467+FoxGalaxy2MishaKhodakov@users.noreply.github.com>
2024-08-22 21:05:55 +0300

    Delete src directory

--- src/script.js
+++
@@ -1,8 +0,0 @@
-// Функция для выполнения поиска
-function performSearch() {
-    const query = document.getElementById('search-query').value;
-    let searchUrl;
-    searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-
-    window.location.href = searchUrl;
-}
