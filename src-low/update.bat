1e808fd6ec2166e5605516ed58172f3fdde42f4e
FoxGalaxy2MishaKhodakov <132774467+FoxGalaxy2MishaKhodakov@users.noreply.github.com>
2024-09-14 10:59:19 +0300

    Update 1.2.10 - New Update System

--- src-low/update.bat
+++ src-low/update.bat
@@ -9,6 +9,21 @@ timeout /t 3 /nobreak >nul
 :: Распаковываем архив update.zip в текущую директорию с заменой файлов
 powershell -Command "Expand-Archive -Path 'update.zip' -DestinationPath '.' -Force"
 
+:: Проверяем, была ли распаковка успешной
+if %errorlevel% neq 0 (
+    echo Ошибка при распаковке файла update.zip
+    exit /b %errorlevel%
+)
+
+:: Удаляем файл update.zip после успешной распаковки
+del "update.zip"
+
+:: Проверяем, была ли операция удаления успешной
+if %errorlevel% neq 0 (
+    echo Ошибка при удалении файла update.zip
+    exit /b %errorlevel%
+)
+
 :: Запускаем файл browser.exe из текущей директории
 start "" "%cd%\browser.exe"
 
