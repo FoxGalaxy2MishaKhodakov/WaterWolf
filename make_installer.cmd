1021225524f4886d41bc4d857d9724f284b1487b
FoxGalaxy2MishaKhodakov <132774467+FoxGalaxy2MishaKhodakov@users.noreply.github.com>
2024-09-20 23:12:50 +0300

    Delete make_installer.cmd

--- make_installer.cmd
+++
@@ -1,1 +0,0 @@
-pyinstaller --noconfirm --onefile --windowed --add-data "C:\Users\MihaB\Desktop\kak\src-installer\background.png;." --add-data "C:\Users\MihaB\Desktop\kak\src-installer\browser.zip;."  "C:\Users\MihaB\Desktop\kak\src-installer\installer.py"
