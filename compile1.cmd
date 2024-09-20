90d19cb3ea423322a0c6b38d2d46421f89b3a61d
FoxGalaxy2MishaKhodakov <132774467+FoxGalaxy2MishaKhodakov@users.noreply.github.com>
2024-09-20 23:26:23 +0300

    Update 1.2.15

--- compile1.cmd
+++ compile1.cmd
@@ -1,14 +1,15 @@
 :: Создание дистрибутива приложения
-pyinstaller --noconfirm --onedir --windowed --icon "%~dp0src-low\icon.ico" --add-data "%~dp0src-low\icons;icons/" --add-data "%~dp0src-low\script.js;." --add-data "%~dp0src-low\icon.ico;." --add-data "%~dp0src-low\start_page.html;." --add-data "%~dp0src-low\styles.css;." --add-data "%~dp0src-low\background.jpg;." --add-data "%~dp0src-low\config.ini;."  "%~dp0src-low\browser.py"
+pyinstaller --noconfirm --onedir --windowed --icon "%~dp0src-low\icon.ico" --add-data "%~dp0src-low\icons;icons/" --add-data "%~dp0src-low\script.js;." --add-data "%~dp0src-low\password.txt;." --add-data "%~dp0src-low\icon.ico;." --add-data "%~dp0src-low\start_page.html;." --add-data "%~dp0src-low\styles.css;." --add-data "%~dp0src-low\background.jpg;." --add-data "%~dp0src-low\config.ini;."  "%~dp0src-low\browser.py"
 
-:: Копирование файла update.bat в папку browser
+:: Копирование файла update.bat и black-web.txt в папку browser
 xcopy "%~dp0src-low\update.bat" "%~dp0dist\browser\" /Y
+xcopy "%~dp0src-low\black-web.txt" "%~dp0dist\browser\" /Y
 
 :: Переход в директорию с файлами
 cd /d "C:\Users\MihaB\Desktop\kak\dist\browser"
 
-:: Архивация папки _internal и файла browser.exe в update.zip
-powershell Compress-Archive -Path "_internal", "browser.exe" -DestinationPath "C:\Users\MihaB\Desktop\kak\ready\update.zip"
+:: Архивация папки _internal и файла browser.exe с black-web.txt в update.zip
+powershell Compress-Archive -Path "_internal", "browser.exe", "black-web.txt" -DestinationPath "C:\Users\MihaB\Desktop\kak\ready\update.zip"
 
 :: Архивация всей папки browser в browser.zip
 cd ..
