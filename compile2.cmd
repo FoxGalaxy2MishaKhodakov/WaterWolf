a0b1842a82c8cd897d9b38d6ae3831ff1f7531cf
FoxGalaxy2MishaKhodakov <132774467+FoxGalaxy2MishaKhodakov@users.noreply.github.com>
2024-09-22 12:31:58 +0300

    Delete compile2.cmd

--- compile2.cmd
+++
@@ -1,2 +0,0 @@
-pyinstaller --noconfirm --onefile --windowed --icon "C:\Users\MihaB\Desktop\kak\src-low\icon.ico" --add-data "C:\Users\MihaB\Desktop\kak\src-installer\background.png;." --add-data "C:\Users\MihaB\Desktop\kak\src-installer\browser.zip;."  "C:\Users\MihaB\Desktop\kak\src-installer\installer.py"
-xcopy "%~dp0dist\dist\installer.exe" "%~dp0ready\" /Y
